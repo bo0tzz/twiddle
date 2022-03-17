3622d11b883e46761a964dc6c43df0bb7e842fb6
bo0tzz <btdewilligen@gmail.com>
2022-03-17 19:45:20 +0100

    Implement ffmpeg chunks concatenation

---
+++ apps/twitch_autodl/lib/twitch_autodl/ffmpeg/concat.ex
@@ -0,0 +1,80 @@
+defmodule TwitchAutodl.FFmpeg.Concat do
+  use GenServer
+  require Logger
+  alias TwitchAutodl.FFmpeg.Concat
+
+  defstruct [:path, :pids, :status, :parent]
+
+  @cmd 'ffmpeg -i index.m3u8 -codec copy index.ts'
+
+  @duration_regex ~r/Duration: (\d*):(\d*):(\d*).(\d*)/
+  @time_regex ~r/time=(\d*):(\d*):(\d*).(\d*)/
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args)
+  end
+
+  @impl true
+  def init({path, parent}) do
+    options = [
+      :stderr,
+      :monitor,
+      cd: String.to_charlist(path)
+    ]
+
+    {:ok, pid, ospid} = :exec.run(@cmd, options)
+
+    {:ok,
+     %Concat{
+       path: path,
+       pids: {pid, ospid},
+       status: %{},
+       parent: parent
+     }}
+  end
+
+  @impl true
+  def handle_info({:stderr, _, data}, state) do
+    state = process_ffmpeg_output(data, state)
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:DOWN, _, :process, _, :normal}, %Concat{parent: parent, path: path} = state) do
+    Logger.info("Finished")
+    send(parent, {:done, path})
+    {:stop, :normal, state}
+  end
+
+  def process_ffmpeg_output("Input #0, " <> _ = output, %Concat{status: status} = state) do
+    duration = Regex.run(@duration_regex, output, capture: :all_but_first)
+               |> as_duration
+    status = Map.put(status, :duration, duration)
+    %{state | status: status}
+  end
+
+  def process_ffmpeg_output("frame= " <> _ = output, %Concat{status: status} = state) do
+    progress = Regex.run(@time_regex, output, capture: :all_but_first)
+    |> as_duration
+    status = Map.put(status, :progress, progress)
+    log_progress(status)
+    %{state | status: status}
+  end
+
+  def process_ffmpeg_output(_output, state), do: state
+
+  def log_progress(status) do
+    with {:ok, duration} <- Map.fetch(status, :duration),
+         {:ok, progress} <- Map.fetch(status, :progress) do
+      percent = Timex.Duration.to_seconds(progress) / Timex.Duration.to_seconds(duration) * 100
+      Logger.info("Progress: #{percent}%")
+    end
+  end
+
+  def as_duration(time) do
+    time
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+    |> Timex.Duration.from_clock()
+  end
+end
