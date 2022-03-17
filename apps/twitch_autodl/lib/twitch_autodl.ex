8fbf555d2b0cd72818d52816acc05525baa5a279
bo0tzz <btdewilligen@gmail.com>
2022-03-17 16:52:54 +0100

    Add (temporary?) helper method for VOD download

--- apps/twitch_autodl/lib/twitch_autodl.ex
+++ apps/twitch_autodl/lib/twitch_autodl.ex
@@ -6,4 +6,14 @@ defmodule TwitchAutodl do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def download_vod(id, base_path) do
+    path = Path.absname(base_path)
+
+    m3u8 = TwitchAutodl.Twitch.Vod.playlist(id)
+    |> TwitchAutodl.M3U8.new()
+
+    TwitchAutodl.Download.download_chunks(m3u8, "chunked", base_path)
+
+  end
 end
