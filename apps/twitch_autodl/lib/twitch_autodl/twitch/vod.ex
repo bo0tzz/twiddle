702c6747d7ebc39c1d407d7f45023aa28e08c29a
bo0tzz <btdewilligen@gmail.com>
2022-03-17 16:52:36 +0100

    Add string conversion for vod id

--- apps/twitch_autodl/lib/twitch_autodl/twitch/vod.ex
+++ apps/twitch_autodl/lib/twitch_autodl/twitch/vod.ex
@@ -3,6 +3,7 @@ defmodule TwitchAutodl.Twitch.Vod do
 
   plug(Tesla.Middleware.BaseUrl, "https://pogu.live/api/get-video/")
 
+  def playlist(vod_id) when is_integer(vod_id), do: Integer.to_string(vod_id) |> playlist()
   def playlist(vod_id) do
     {:ok, resp} = get(vod_id <> ".m3u8")
     resp.body
